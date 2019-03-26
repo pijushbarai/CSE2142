32f18e0214e104a9c377bbb0da066cf9927b7465
Sajeeb Chakraborty <sajeebchakraborty.cse2000@gmail.com>
2019-03-26 23:40:57 +0600

    Division

--- Addition.java
+++ Addition.java
@@ -1,5 +1,9 @@
 public class Addition{
 	public static void main(String[] args) {
 		public void add(int x, int y);
+		
+		return x+y;
+
 	}
+	
 }
--- javaCalc.java
+++ javaCalc.java
@@ -1,5 +1,9 @@
 public class javaCalc{
 	public static void public static void main(String[] args) {
 		
+		public void add(int x, int y);
+		
+		return x/y;
+		
 	}
 }
